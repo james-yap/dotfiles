f78a09f50a666921cd6034423d1dc5d95bdc2acb
James Y. <james.yap@shopify.com>
2026-05-21 13:35:48 -0400

    add commented out config block to disable diffview completions and add example comment

--- stow/neovim/.config/nvim/lua/plugins/diffview.lua
+++ stow/neovim/.config/nvim/lua/plugins/diffview.lua
@@ -3,5 +3,25 @@ return {
   {
     "sindrets/diffview.nvim",
     cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    -- To disable command-line completion for Diffview commands (highly recommended in massive monoliths
+    -- like shop/world to prevent Neovim from freezing or running expensive Git queries):
+    -- Uncomment the config block below.
+    --
+    -- Example use case for DiffviewOpen (for future reference):
+    -- :DiffviewOpen main...pr-feature-branch -- . ':!**/tests/**' ':!**/translations/**'
+    --
+    -- config = function(_, opts)
+    --   require("diffview").setup(opts)
+    --
+    --   local diffview = require("diffview")
+    --
+    --   diffview.completers.DiffviewOpen = function()
+    --     return {}
+    --   end
+    --
+    --   diffview.completers.DiffviewFileHistory = function()
+    --     return {}
+    --   end
+    -- end,
   },
 }
