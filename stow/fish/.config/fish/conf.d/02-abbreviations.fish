9b2cdeb15e24a7d9cd642662f938a3c975e3feec
James Yap <contact@jamesyap.org>
2026-05-31 13:30:21 -0400

    feat: fish abbreviations

---
+++ stow/fish/.config/fish/conf.d/02-abbreviations.fish
@@ -0,0 +1,11 @@
+# navigation
+abbr lah "lsd -lah"
+abbr cd z
+
+# git
+abbr lg lazygit
+abbr gst "git status"
+abbr jcm "jj commit -m"
+abbr jgp "jj git push"
+abbr jgf "jj git fetch"
+abbr jbs "jj bookmark set main -r @-"
