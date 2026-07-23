d2631d69a45f74f40f56fbd64bb7e0e2fcca3ab4
James Y. <james.yap@shopify.com>
2026-07-23 11:23:19 -0400

    fish: add tnvim abbr for Diffview excluding tests
    Assisted-By: devx/684650e2-7cbd-4fbe-94d2-c81241112533

--- stow/fish/.config/fish/conf.d/02-abbreviations.fish
+++ stow/fish/.config/fish/conf.d/02-abbreviations.fish
@@ -9,3 +9,6 @@ abbr jcm "jj commit -m"
 abbr jgp "jj git push"
 abbr jgf "jj git fetch"
 abbr jbs "jj bookmark set main -r @-"
+
+# nvim
+abbr tnvim 'nvim -c "DiffviewOpen origin/main...HEAD -- . :!*test*"'
