1e233e095d57be901300053a35988f4d05780be6
James Yap <contact@jamesyap.org>
2026-07-25 15:10:12 -0400

    added 'quick' abbr

--- stow/fish/.config/fish/conf.d/02-abbreviations.fish
+++ stow/fish/.config/fish/conf.d/02-abbreviations.fish
@@ -12,3 +12,6 @@ abbr jbs "jj bookmark set main -r @-"
 
 # nvim
 abbr tnvim 'nvim -c "DiffviewOpen origin/main...HEAD -- . :!*test*"'
+
+# ai
+abbr quick 'omp --model "openai-codex/gpt-5.6-luna" --thinking "off"'
